ba88f37531ff9a091e428e96e0142882d5c620a7
Ubuntu <ubuntu@ip-10-254-10-60.us-west-2.compute.internal>
2013-03-09 02:17:50 +0000

    Added hedera.py

--- hedera.py
+++ hedera.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 
 from mininet.topo import Topo
 from mininet.node import Controller, RemoteController, OVSKernelSwitch, CPULimitedHost
@@ -10,7 +8,7 @@ from mininet.util import custom
 from mininet.log import setLogLevel, info, warn, error, debug
 
 from DCTopo import FatTreeTopo, NonBlockingTopo
-from routing import HashedRouting
+#from routing import HashedRouting
 
 from subprocess import Popen, PIPE
 from argparse import ArgumentParser
@@ -35,76 +33,73 @@ parser.add_argument('-t', '--time', dest='time', type=int, default=30,
 parser.add_argument('-p', '--cpu', dest='cpu', type=float, default=-1,
         help='cpu fraction to allocate to each host')
 
+parser.add_argument('-n', '--nonblocking', dest='nonblocking', default=False,
+        action='store_true', help='Run the experiment on the noneblocking topo')
+
 args = parser.parse_args()
 
 def NonBlockingNet(k=4, bw=100, cpu=-1, queue=100):
-    ''' Create a NonBlocking Net ''' 
-    
+    ''' Create a NonBlocking Net '''
+
     topo = NonBlockingTopo(k)
     host = custom(CPULimitedHost, cpu=cpu)
     link = custom(TCLink, bw=bw, max_queue_size=queue)
 
     net = Mininet(topo, host=host, link=link, switch=OVSKernelSwitch,
             controller=Controller)
-   
+
     return net
 
 def FatTreeNet(k=4, bw=100, cpu=-1, queue=100):
     ''' Create a Fat-Tree network '''
 
-    pox_c = Popen("~/pox/pox.py ECMPController --topo=ft", shell=True)
-    
-    info('** Creating the topology')
+    pox_c = Popen("~/pox/pox.py DCController --topo=ft --routing=ECMP", shell=True)
+
+    info('*** Creating the topology')
     topo = FatTreeTopo(k, speed=bw/1000)
+
     host = custom(CPULimitedHost, cpu=cpu)
     link = custom(TCLink, bw=bw, max_queue_size=queue)
-    
+
     net = Mininet(topo, host=host, link=link, switch=OVSKernelSwitch,
             controller=RemoteController)
 
     return net
 
 def trafficGen(args, hosts, net):
-    ''' '''
+    ''' Run the traffic generator and monitor all of the interfaces '''
     listen_port = 12345
     sample_period_us = 1000000
 
     traffic_gen = 'cluster_loadgen/loadgen'
     if not os.path.isfile(traffic_gen):
-        error('The traffic generator doesn\'t exist. \ncd \
-                hedera/cluster_loadgen; make')
+        error('The traffic generator doesn\'t exist. \ncd hedera/cluster_loadgen; make\n')
         return
 
-    info('** Starting load-generators\n')
+    info('*** Starting load-generators\n')
     for h in hosts:
         tg_cmd = '%s -f %s -i %s -l %d -p %d 2&>1 > %s/%s.out &' % (traffic_gen,
                 args.input_file, h.defaultIntf(), listen_port, sample_period_us,
                 args.output_dir, h.name)
         h.cmd(tg_cmd)
-   
-    print h.defaultIntf()
-    print args.input_file
-    print listen_port
-    print sample_period_us
-    
+
     sleep(1)
 
-    info('** Triggering load-generators\n')
+    info('*** Triggering load-generators\n')
     for h in hosts:
         h.cmd('nc -nzv %s %d' % (h.IP(), listen_port))
 
 
     monitor = multiprocessing.Process(target = monitor_devs_ng, args =
         ('%s/rate.txt' % args.output_dir, 0.01))
-    
+
     monitor.start()
 
-    #sleep(args.time)
-    sleep(5)
+    sleep(args.time)
 
     monitor.terminate()
 
-    info('** Stopping load-generators\n')
+    info('*** Stopping load-generators\n')
     for h in hosts:
         h.cmd('killall loadgen')
 
@@ -113,31 +108,32 @@ def ECMPTest(args):
     bw = 100
     queue = 100
     net = FatTreeNet( k=k, cpu=args.cpu, bw=bw, queue=queue)
-    
+    net.start()
+
     # wait for the switches to connect to the controller
     info('** Waiting for switches to connect to the controller\n')
     sleep(5)
-    
-    net.start()
-    print "** Start Pinging"
-    net.pingPair() 
+
+    net.pingAll()
+
+    hosts = net.hosts
+    trafficGen(args, hosts, net)
+
     net.stop()
 
 def NonBlockingTest(args):
     k = 4
     bw = 100
     queue = 100
-    net = NonBlockingNet(k=k, cpu=args.cpu, bw=bw, queue=queue)    
+    net = NonBlockingNet(k=k, cpu=args.cpu, bw=bw, queue=queue)
     net.start()
-    
+
     info('** Waiting for switches to connect to the controller\n')
     sleep(1)
 
     net.pingAll()
 
     hosts = net.hosts
-
-
     trafficGen(args, hosts, net)
 
     net.stop()
@@ -157,17 +153,20 @@ def clean():
             pass
 
 if __name__ == '__main__':
-    
+
     setLogLevel( 'info' )
     if not os.path.exists(args.output_dir):
         print args.output_dir
         os.makedirs(args.output_dir)
 
     clean()
-    
-    NonBlockingTest(args)
-    #ECMPTest(args)
+
+    if args.nonblocking:
+        NonBlockingTest(args)
+    else:
+        ECMPTest(args)
 
     clean()
 
-    os.system('sudo mn -c') 
+    Popen("killall -9 top bwm-ng", shell=True).wait()
+    os.system('sudo mn -c')
